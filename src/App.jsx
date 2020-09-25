67c519d28e96cf26943288d4c7217a301ddacc42
Talsh <talshalom900@gmail.com>
2020-09-25 17:46:02 +0300

    A Covid-19 Tracker
    [X] - This simple tracker shows the amount of infected and recovered
    cases of covid-19, along with the number of due to covid-19.
    [X] - The tracker shows the a chart with the active cases for each
    country in the world, and globally.
    [X] - Fully responsive, using Material UI.

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import styles from './App.module.css';
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api';
+import coronaLogo from './images/covid.png';
+
+class App extends React.Component {
+    state = {
+        data: {},
+        country: ''
+    }
+
+    async componentDidMount() {
+        const fetchedData = await fetchData();
+        this.setState({ data: fetchedData });
+    }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+        this.setState({ data: fetchedData, country: country });
+    }
+    render() {
+        const { data, country } = this.state;
+        return (
+            <div className={styles.container}>
+                <img className={styles.image} src={coronaLogo} alt='COVID-19' />
+                <Cards data={data} />
+                <CountryPicker handleCountryChange={this.handleCountryChange} />
+                <Chart data={data} country={country} />
+            </div>
+        )
+    }
+}
+
+export default App;
